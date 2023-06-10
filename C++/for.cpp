701c0e6ebf1baa774da467eedc947a07e35ae702
Seungje Lim <sjlim1217@naver.com>
2023-06-11 01:50:52 +0900

    add

--- C++/for.cpp
+++ C++/for.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
-	for (int i = 0; i < 10; i++) {
-
+	for (int i = 0; i < 100; i++) 
+	{
 		if (i == 3)
-			continue; // i가 3일 때만 건너뒴
+			continue; // i가 3일 때만 건너뜀
+		if (i==4)
+			continue; // i가 4일 때만 건너뜀
 
 		std::cout << i << std::endl;
 	}
