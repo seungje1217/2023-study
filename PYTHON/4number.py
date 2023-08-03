23fe3d8dda4279f40f00dcb325d6d07158690145
Seungje Lim <sjlim1217@naver.com>
2023-08-03 15:39:17 +0900

    add

---
+++ PYTHON/4number.py
@@ -0,0 +1,16 @@
+# num1=int(input("첫 번째 정수 : "))+100
+# num2=int(input("두 번째 정수 : "))%10
+# num3=int(input("세 번째 정수 : "))/10
+# num4=int(input("네 번째 정수 : "))**10
+#
+# print("num1 : %d" % num1)
+# print("num2 : %d" % num2)
+# print("num3 : %d" % num3)
+# print("num4 : %d" % num4)
+
+
+# 일회성
+print(int(input("정수 1 입력 : "))+100)
+print(int(input("정수 2 입력 : "))%10)
+print(int(input("정수 3 입력 : "))/10)
+print(int(input("정수 4 입력 : "))**10)
