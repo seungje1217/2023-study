675d4481f017938aacf3b26c60d2700f09d71827
Seungje Lim <sjlim1217@naver.com>
2023-08-03 13:41:30 +0900

    add

--- PYTHON/score.py
+++ PYTHON/score.py
@@ -1,8 +1,5 @@
-num1=int(input("첫 번째 정수 입력 : "))
-num2=int(input("두 번째 정수 입력 : "))
+num1=int(input("첫 번째 정수 입력 : "))+100
+num2=int(input("두 번째 정수 입력 : "))%10
 
-num11=num1+100
-num22=num2%10
-
-print("첫 번째 정수 : %d"% num11)
-print("두 번째 정수 : %d"% num22)
+print("첫 번째 정수 : %d"% num1)
+print("두 번째 정수 : %d"% num2)
